3429e0ceb1f509ca663db3003f80a4694723312d
Juanen100 <65170591+Juanen100@users.noreply.github.com>
2025-01-26 13:19:41 +0100

    oops

--- src/themeSelector.cpp
+++ src/themeSelector.cpp
@@ -12,46 +12,43 @@
 #include <wups/storage.h>
 
 bool ReplaceContent(const std::string men, const std::string men2, const std::string cafeBarista) {
-    std::string layerName = "StyleMiiU /vol/" + subdir;
+    std::string layerName = "StyleMiiU /vol/content";
 
     bool oneRedirect = false;
-    if(!_menPackPath.empty()){
+    if(!men.empty()){
         auto res = ContentRedirection_AddFSLayer(&gContentLayerHandle,
                                                  layerName.c_str(),
-                                                 _menPackPath.c_str(),
+                                                 men.c_str(),
                                                  FS_LAYER_TYPE_CONTENT_MERGE,
                                                  "/vol/content/Common/Package");
         if (res == CONTENT_REDIRECTION_RESULT_SUCCESS) {
-            DEBUG_FUNCTION_LINE("Redirect /vol/%s to %s", subdir.c_str(), _menPackPath.c_str());
             oneRedirect = true;
         } else {
-            DEBUG_FUNCTION_LINE_ERR("Failed to redirect /vol/%s to %s", subdir.c_str(), _menPackPath.c_str());
+            DEBUG_FUNCTION_LINE_ERR("Failed to redirect /vol/content to %s", men.c_str());
         }
     }
-    if(!_men2PackPath.empty()){
+    if(!men2.empty()){
         auto res = ContentRedirection_AddFSLayer(&gContentLayerHandle,
                                                  layerName.c_str(),
-                                                 _men2PackPath.c_str(),
+                                                 men2.c_str(),
                                                  FS_LAYER_TYPE_CONTENT_MERGE,
                                                  "/vol/content/Common/Package");
         if (res == CONTENT_REDIRECTION_RESULT_SUCCESS) {
-            DEBUG_FUNCTION_LINE("Redirect /vol/%s to %s", subdir.c_str(), _men2PackPath.c_str());
             oneRedirect = true;
         } else {
-            DEBUG_FUNCTION_LINE_ERR("Failed to redirect /vol/%s to %s", subdir.c_str(), _men2PackPath.c_str());
+            DEBUG_FUNCTION_LINE_ERR("Failed to redirect /vol/content to %s", men2.c_str());
         }
     }
-    if(!_cafeBaristaMenPath.empty()){
+    if(!cafeBarista.empty()){
         auto res = ContentRedirection_AddFSLayer(&gContentLayerHandle,
                                                  layerName.c_str(),
-                                                 _cafeBaristaMenPath.c_str(),
+                                                 cafeBarista.c_str(),
                                                  FS_LAYER_TYPE_CONTENT_MERGE,
                                                  "/vol/content/Common/Sound/Men");
         if (res == CONTENT_REDIRECTION_RESULT_SUCCESS) {
-            DEBUG_FUNCTION_LINE("Redirect /vol/%s to %s", subdir.c_str(), _cafeBaristaMenPath.c_str());
             oneRedirect = true;
         } else {
-            DEBUG_FUNCTION_LINE_ERR("Failed to redirect /vol/%s to %s", subdir.c_str(), _cafeBaristaMenPath.c_str());
+            DEBUG_FUNCTION_LINE_ERR("Failed to redirect /vol/content to %s", cafeBarista.c_str());
         }
     }
     return oneRedirect;
